2be3ac120ee59b218d4a327923ccc46baa629c90
Tim <timwagner997@gmail.com>
2022-10-12 19:52:30 +0200

    added a few assets to the game

--- core/src/com/mygdx/game/DropGame.java
+++ core/src/com/mygdx/game/DropGame.java
@@ -12,7 +12,7 @@ public class DropGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bucket.png");
 	}
 
 	@Override
